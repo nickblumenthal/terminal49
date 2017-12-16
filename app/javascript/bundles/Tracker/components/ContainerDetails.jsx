359097d34ac207426f99ce18ded423f8892c2f53
Nick Blumenthal <nrblum01@gmail.com>
2017-12-16 14:44:19 -0800

    Adds container detail component

--- app/javascript/bundles/Tracker/components/ContainerDetails.jsx
+++ app/javascript/bundles/Tracker/components/ContainerDetails.jsx
@@ -4,13 +4,45 @@ import {Card, Grid, Header} from 'semantic-ui-react'
 
 const ContainerDetails = ({containers}) => {
   const renderContainers = (containers) => {
-    return (
-        <Card>
-          <Card.Header>
-            hi
-          </Card.Header>
-        </Card>
-    )
+    return containers.map((container) => {
+      return (
+          <Card fluid color="blue">
+            <Card.Header textAlign="center">
+              <Header as="h2">Container {container.get('number')}</Header>
+            </Card.Header>
+            <Card.Content>
+              <Grid columns={4} divided stackable>
+                <Grid.Row>
+                  <Grid.Column>
+                    <Header as="h2">B/L Number</Header>
+                    {container.get('number')}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as="h2">Steamship Line</Header>
+                    {container.get('size')}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as="h2">Origin</Header>
+                    {container.get('type')}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as="h2">Destination</Header>
+                    {container.get('last_status')}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as="h2">Destination</Header>
+                    {container.get('location')}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as="h2">Destination</Header>
+                    {container.get('last_status_at')}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Card.Content>
+          </Card>
+      )
+    })
   };
 
   return (
