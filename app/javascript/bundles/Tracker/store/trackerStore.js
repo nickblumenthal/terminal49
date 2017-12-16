10d99e2a0ddcd6f2f57134ff78bd18077e9ddf0f
Nick Blumenthal <nrblum01@gmail.com>
2017-12-15 22:48:43 -0800

    Add redux dev tools

--- app/javascript/bundles/Tracker/store/trackerStore.js
+++ app/javascript/bundles/Tracker/store/trackerStore.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk'
 import TrackerReducer from '../reducers/trackerReducer';
 
 const configureStore = (railsProps) => (
-  createStore(TrackerReducer, railsProps, applyMiddleware(thunk))
+  createStore(TrackerReducer, railsProps, composeWithDevTools(applyMiddleware(thunk)))
 );
 
 export default configureStore;
