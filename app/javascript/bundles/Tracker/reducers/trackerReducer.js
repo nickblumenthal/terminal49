20dc6390097fbc05c3a37840090cd4425a60e43d
Nick Blumenthal <nrblum01@gmail.com>
2017-12-16 14:31:03 -0800

    Remove unused autogen code

--- app/javascript/bundles/Tracker/reducers/trackerReducer.js
+++ app/javascript/bundles/Tracker/reducers/trackerReducer.js
@@ -2,15 +2,6 @@ import { combineReducers } from 'redux';
 import { HELLO_WORLD_NAME_UPDATE } from '../constants/trackerConstants';
 import bookingReducer from './bookingReducer'
 
-const name = (state = '', action) => {
-  switch (action.type) {
-    case HELLO_WORLD_NAME_UPDATE:
-      return action.text;
-    default:
-      return state;
-  }
-};
-
-const TrackerReducer = combineReducers({ name, bookingReducer });
+const TrackerReducer = combineReducers({ bookingReducer });
 
 export default TrackerReducer;
