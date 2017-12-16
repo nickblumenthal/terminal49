e2651be4194cc138955a5ef4188c4b6f3fb71943
Nick Blumenthal <nrblum01@gmail.com>
2017-12-15 23:05:44 -0800

    Utilize json property on response to retrieve data

--- app/javascript/bundles/Tracker/actions/bookingActionCreators.js
+++ app/javascript/bundles/Tracker/actions/bookingActionCreators.js
@@ -1,29 +1,29 @@
 import fetch from 'cross-fetch'
 import Booking from '../constants/bookingConstants'
 
-export function fetchBooking(id) {
+export function fetchBooking(bookingId) {
   return function(dispatch) {
-    dispatch(requestBooking(id));
-    fetch(`/bookings/${id}`).then((response) => {
-      dispatch(receiveBooking(id, response.data))
-    })
+    dispatch(requestBooking(bookingId));
+    fetch(`/bookings/${bookingId}`)
+        .then((response) => response.json())
+        .then((data) => dispatch(receiveBooking(bookingId, data)))
   }
 }
 
-export function requestBooking(id) {
+export function requestBooking(bookingId) {
   return {
     type: Booking.REQUEST_BOOKING,
     data: {
-      id
+      bookingId
     }
   }
 }
 
-export function receiveBooking(id, bookingData) {
+export function receiveBooking(bookingId, bookingData) {
   return {
     type: Booking.RECEIVE_BOOKING,
     data: {
-      id,
+      bookingId,
       bookingData
     }
   }
