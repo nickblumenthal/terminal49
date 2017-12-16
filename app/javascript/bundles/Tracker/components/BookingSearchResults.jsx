ea60d45d4b0fd65c7e69048016ae9d533b09d025
Nick Blumenthal <nrblum01@gmail.com>
2017-12-16 10:48:43 -0800

    Add booking search results component

--- app/javascript/bundles/Tracker/components/BookingSearchResults.jsx
+++ app/javascript/bundles/Tracker/components/BookingSearchResults.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Card, Grid, Header } from 'semantic-ui-react'
 
 const BookingSearchResults = ({bookings}) => {
-  return bookings.map((booking) => {
-    <div>{booking.booking_number}</div>
-  })
+  const renderCards = (bookings) => {
+    return bookings.valueSeq().map((booking) => {
+      return(
+        <Card fluid color="red">
+          <Card.Content>
+            <Grid columns={5} divided stackable>
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as="h2">B/L Number</Header>
+                  {booking.get('booking_number')}
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as="h2">Steamship Line</Header>
+                  {booking.get('steamship_line')}
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as="h2">Origin</Header>
+                  {booking.get('origin')}
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as="h2">Destination</Header>
+                  {booking.get('destination')}
+                </Grid.Column>
+                <Grid.Column>
+                  <Link to={`/bookings/${booking.get('booking_number')}`}>
+                    View Detail
+                  </Link>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Card.Content>
+        </Card>
+      )
+    })
+  };
+
+  return (
+      <Card.Group>
+        {renderCards(bookings)}
+      </Card.Group>
+  )
 };
 
 export default BookingSearchResults
--- app/javascript/bundles/Tracker/components/Tracker.jsx
+++ app/javascript/bundles/Tracker/components/Tracker.jsx
@@ -1,24 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import { Button, Checkbox, Form } from 'semantic-ui-react'
+import {Button, Container, Form, Header} from 'semantic-ui-react'
 import BookingSearchResults from './BookingSearchResults'
 
 export default class Tracker extends React.Component {
   static propTypes = {
     bookings: PropTypes.object
   };
-  
+
   constructor(props) {
     super(props);
 
     // How to set initial state in ES6 class syntax
     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { bookingNumber: '' };
+    this.state = {bookingNumber: ''};
   }
 
   handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+    this.setState({[e.target.name]: e.target.value});
   };
 
   handleSubmit = () => {
@@ -27,15 +27,18 @@ export default class Tracker extends React.Component {
   };
 
   render() {
-    return [
-        <Form className="search" onSubmit={this.handleSubmit}>
-          <Form.Field>
-            <label>Booking Number</label>
-            <input placeholder='Booking Number' name="bookingNumber" onChange={this.handleChange}/>
-          </Form.Field>
-          <Button type='submit'>Search</Button>
-        </Form>,
-        <BookingSearchResults bookings={this.props.bookings} />
-    ];
+    return (
+        <Container>
+          <Header as="h1" textAlign="center">Trackster</Header>
+          <Form className="search" onSubmit={this.handleSubmit}>
+            <Form.Field>
+              <label>Booking Number</label>
+              <input placeholder='Booking Number' name="bookingNumber" onChange={this.handleChange}/>
+            </Form.Field>
+            <Button type='submit'>Search</Button>
+          </Form>
+          <BookingSearchResults bookings={this.props.bookings}/>
+        </Container>
+    );
   }
 }
