e7d0dd501aeba3ee0f01c191a9fd482e111fa4b5
Nick Blumenthal <nrblum01@gmail.com>
2017-12-16 14:30:42 -0800

    Make hydrated rails props immutable

--- app/javascript/bundles/Tracker/store/trackerStore.js
+++ app/javascript/bundles/Tracker/store/trackerStore.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import Immutable from 'immutable'
 import thunk from 'redux-thunk'
 import TrackerReducer from '../reducers/trackerReducer';
 
 const configureStore = (railsProps) => (
-  createStore(TrackerReducer, railsProps, composeWithDevTools(applyMiddleware(thunk)))
+  createStore(TrackerReducer, makeImmutable(railsProps), composeWithDevTools(applyMiddleware(thunk)))
 );
+
+// Prior state is hydrated from plain rails object - we need to make the values immutable
+const makeImmutable = (priorState) => {
+  Object.keys(priorState).map((key, index) => {
+    priorState[key] = Immutable.fromJS(priorState[key])
+  });
+  console.log(priorState);
+  return priorState;
+};
 
 export default configureStore;
