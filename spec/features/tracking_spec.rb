9e2bdda50ba88bae96ec0d08285ad723e8f536fc
Nick Blumenthal <nrblum01@gmail.com>
2017-12-20 21:53:09 -0800

    Fix bad specs

--- spec/features/tracking_spec.rb
+++ spec/features/tracking_spec.rb
@@ -4,43 +4,76 @@ require 'rails_helper'
 # specs, but I'm going to ignore most edge causes for the sake of time.
 
 RSpec.describe 'Tracking a container', js: true do
-  it 'shows the user a form to search for containers' do
-    visit '/'
-    expect(page).to have_content('Search')
-  end
-
-  it 'allows the user to search for a container via the search form and then save it' do
-    visit '/'
-    expect(page).to have_css('input[name="bookingNumber"]')
-    fill_in 'input[name="bookingNumber"]', visible: false, with: 'PABVTXG790195100'
-    click_button 'Search'
-
-    expect(page).to have_content('B/L Number: TXG790195200')
-
-    click_button 'Save'
-    expect(page).to have_content('Remove')
-  end
-
-  it 'allows the user to signup' do
-    expect{
+  context 'when logged out' do
+    it 'shows the user a form to search for containers' do
       visit '/'
-      click_button 'Sign Up'
+      expect(page).to have_content('Search')
+    end
+
+    it 'allows the user to search for a container via the search form' do
+      visit '/'
+      expect(page).to have_css('input[name="bookingNumber"]')
+      fill_in 'bookingNumber', visible: false, with: 'TXG790195100'
+      click_button 'Search'
+
+      expect(page).to have_content('B/L Number TXG790195100')
+    end
+
+    it 'allows users to direct navigate to bookings' do
+      visit '/bookings/TXG790195100'
+
+      expect(page).to have_content('B/L Number TXG790195100')
+    end
+
+    it 'allows the user to signup' do
+      expect{
+        visit '/'
+        click_button 'Sign Up'
+        expect(page).to have_content('Email')
+        fill_in 'email', with: 'nick@gmail.com'
+        fill_in 'password', with: 'nicknick'
+        click_button 'Submit'
+        expect(page).to have_content('Logout')
+      }.to change{User.count}.by(1)
+    end
+
+    it 'allows the user to login' do
+      User.create(email: 'nick@gmail.com', password: 'nicknick')
+      visit '/'
+      click_button 'Login'
       expect(page).to have_content('Email')
-      fill_in 'input[name="email"]', with: 'nick@gmail.com'
-      fill_in 'input[name="password"]', with: 'nicknick'
+      fill_in 'email', with: 'nick@gmail.com'
+      fill_in 'password', with: 'nicknick'
       click_button 'Submit'
-    }.to change{User.count}.by(1)
+
+      expect(page).to have_content('Search History')
+    end
+
+    it 'prompts the user to sign in if they attempt to save a booking' do
+      visit '/'
+
+      fill_in 'bookingNumber', visible: false, with: 'TXG790195100'
+      click_button 'Search'
+      expect(page).to have_content('B/L Number TXG790195100')
+      click_button 'Save'
+
+      expect(page).to have_content('Please login')
+    end
   end
 
-  it 'allows the user to login' do
-    User.create(email: 'nick@gmail.com', password: 'nicknick')
-    visit '/'
-    click_button 'Login'
-    expect(page).to have_content('Email')
-    fill_in 'input[name="email"]', with: 'nick@gmail.com'
-    fill_in 'input[name="password"]', with: 'nicknick'
-    click_button 'Submit'
+  context 'when logged in' do
+    it 'allows the user to save a booking' do
+      User.create(email: 'nick@gmail.com', password: 'nicknick')
+      visit '/bookings/TXG790195100'
+      click_button 'Login'
+      expect(page).to have_content('Email')
+      fill_in 'email', with: 'nick@gmail.com'
+      fill_in 'password', with: 'nicknick'
+      click_button 'Submit'
+      expect(page).to have_content('TXG790195100')
 
-    expect(page).to have_content('Search History')
+      click_button 'Save'
+      expect(page).to have_content('Remove')
+    end
   end
 end
